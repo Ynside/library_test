9d8303ea8d65e697c4cf0c4286ed163c294fb4a4
Ynside <hsn_193@hotmail.com>
2023-03-11 02:14:59 +0300

    libraryV0.1
    main

---
+++ library/Form2.Designer.cs
@@ -0,0 +1,382 @@
+﻿namespace library
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.directoryEntry1 = new System.DirectoryServices.DirectoryEntry();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.mNO = new System.Windows.Forms.TextBox();
+            this.mName = new System.Windows.Forms.TextBox();
+            this.kNO1 = new System.Windows.Forms.TextBox();
+            this.deadline = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.kNO2 = new System.Windows.Forms.TextBox();
+            this.kName = new System.Windows.Forms.TextBox();
+            this.stok = new System.Windows.Forms.TextBox();
+            this.azar = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.ekleBtnM = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(1, 217);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(398, 221);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // mNO
+            // 
+            this.mNO.CausesValidation = false;
+            this.mNO.Location = new System.Drawing.Point(91, 38);
+            this.mNO.Name = "mNO";
+            this.mNO.Size = new System.Drawing.Size(100, 20);
+            this.mNO.TabIndex = 1;
+            // 
+            // mName
+            // 
+            this.mName.Location = new System.Drawing.Point(91, 79);
+            this.mName.Name = "mName";
+            this.mName.Size = new System.Drawing.Size(100, 20);
+            this.mName.TabIndex = 2;
+            // 
+            // kNO1
+            // 
+            this.kNO1.Location = new System.Drawing.Point(91, 119);
+            this.kNO1.Name = "kNO1";
+            this.kNO1.Size = new System.Drawing.Size(100, 20);
+            this.kNO1.TabIndex = 3;
+            // 
+            // deadline
+            // 
+            this.deadline.Location = new System.Drawing.Point(91, 158);
+            this.deadline.Name = "deadline";
+            this.deadline.Size = new System.Drawing.Size(100, 20);
+            this.deadline.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Müşteri NO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "İsim ve Soyisim";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Kitap NO";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Kiralama Süresi";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // kNO2
+            // 
+            this.kNO2.Location = new System.Drawing.Point(574, 38);
+            this.kNO2.Name = "kNO2";
+            this.kNO2.Size = new System.Drawing.Size(100, 20);
+            this.kNO2.TabIndex = 9;
+            // 
+            // kName
+            // 
+            this.kName.Location = new System.Drawing.Point(574, 79);
+            this.kName.Name = "kName";
+            this.kName.Size = new System.Drawing.Size(100, 20);
+            this.kName.TabIndex = 10;
+            // 
+            // stok
+            // 
+            this.stok.Location = new System.Drawing.Point(574, 119);
+            this.stok.Name = "stok";
+            this.stok.Size = new System.Drawing.Size(100, 20);
+            this.stok.TabIndex = 11;
+            // 
+            // azar
+            // 
+            this.azar.Location = new System.Drawing.Point(574, 158);
+            this.azar.Name = "azar";
+            this.azar.Size = new System.Drawing.Size(100, 20);
+            this.azar.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(88, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(105, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Müşteri Kayıt Bölümü";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(571, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(107, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Kitap Ekleme Bölümü";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(490, 45);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(50, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Kitap NO";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(490, 86);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(52, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Kitap İsmi";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(490, 126);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(29, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Stok";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(490, 165);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(34, 13);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "Yazar";
+            // 
+            // ekleBtnM
+            // 
+            this.ekleBtnM.Location = new System.Drawing.Point(49, 188);
+            this.ekleBtnM.Name = "ekleBtnM";
+            this.ekleBtnM.Size = new System.Drawing.Size(75, 23);
+            this.ekleBtnM.TabIndex = 19;
+            this.ekleBtnM.Text = "EKLE";
+            this.ekleBtnM.UseVisualStyleBackColor = true;
+            this.ekleBtnM.Click += new System.EventHandler(this.ekleBtnM_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(160, 188);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "GUNCELLE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(525, 188);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 21;
+            this.button3.Text = "EKLE";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(643, 188);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 22;
+            this.button4.Text = "GUNCELLE";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(260, 112);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(162, 20);
+            this.textBox9.TabIndex = 23;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(295, 9);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(94, 13);
+            this.label11.TabIndex = 24;
+            this.label11.Text = "Müşteri Sorgulama";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(284, 86);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(115, 13);
+            this.label12.TabIndex = 25;
+            this.label12.Text = "Müşteri İsmi ve Soyismi";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(421, 216);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(416, 221);
+            this.dataGridView2.TabIndex = 26;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(347, 177);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 27;
+            this.button1.Text = "EKLE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(849, 449);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.ekleBtnM);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.azar);
+            this.Controls.Add(this.stok);
+            this.Controls.Add(this.kName);
+            this.Controls.Add(this.kNO2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.deadline);
+            this.Controls.Add(this.kNO1);
+            this.Controls.Add(this.mName);
+            this.Controls.Add(this.mNO);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.DirectoryServices.DirectoryEntry directoryEntry1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox mName;
+        private System.Windows.Forms.TextBox kNO1;
+        private System.Windows.Forms.TextBox deadline;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox kNO2;
+        private System.Windows.Forms.TextBox kName;
+        private System.Windows.Forms.TextBox stok;
+        private System.Windows.Forms.TextBox azar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button ekleBtnM;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox mNO;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ library/Form2.cs
@@ -0,0 +1,260 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace library
+{
+    public partial class Form2 : Form
+    {
+        string[] kontrol = new string[8];
+
+        public void veriCek()
+        {
+            adapter = new MySqlDataAdapter("SELECT * FROM librarypanel2 ", mysqlbaglan);
+            MySqlCommand command = new MySqlCommand(
+          "SELECT * FROM librarypanel2" ,
+          mysqlbaglan);
+
+
+            MySqlDataReader reader = command.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                reader.Read();
+                if (reader.Read() != false) 
+                {
+                    for (int i = 0; i <= reader.Depth; i++)
+                    {
+
+
+                        kontrol[i] = Convert.ToString(reader[i]);
+
+                    }
+                }else
+                {
+                    MessageBox.Show("Database bağlantı hatası!");
+                }
+                
+ 
+            }
+            else
+            {
+                MessageBox.Show("Hata! Sütun yok.");
+            }
+            reader.Close();
+
+        }
+
+        public MySqlConnection mysqlbaglan = new MySqlConnection("Server=localhost;Database=library;Uid=root;Pwd='';");
+        MySqlCommand cmd;
+        MySqlDataAdapter adapter;
+        MySqlDataAdapter adana;
+        DataTable dt;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                mysqlbaglan.Open();
+                if (mysqlbaglan.State != ConnectionState.Closed)
+                {
+                    MessageBox.Show("Bağlantı Başarılı Bir Şekilde Gerçekleşti");
+                }
+                else
+                {
+                    MessageBox.Show("Maalesef Bağlantı Yapılamadı...!");
+                }
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Hata! " + err.Message, "Hata Oluştu", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            VeriGetirM();
+            VeriGetirK();
+            veriCek();
+
+
+
+        }
+        public void veriEkleM()
+        {
+            string sorgu = "Insert into librarypanel (musteriNO,musteriAD,kitapNO,kiralamaSuresi) values (@mNo,@mName,@kNO,@deadline)";
+
+
+            cmd = new MySqlCommand(sorgu, mysqlbaglan);
+            cmd.Parameters.AddWithValue("@mNO", mNO.Text);
+            cmd.Parameters.AddWithValue("@mName", mName.Text);
+            cmd.Parameters.AddWithValue("@kNO", kNO1.Text);
+            cmd.Parameters.AddWithValue("@deadline", deadline.Text);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Eklendi!");
+            
+
+        }
+        public void VeriGetirM()
+        {
+            dt = new DataTable();
+            
+            adapter = new MySqlDataAdapter("SELECT *FROM librarypanel", mysqlbaglan);
+            adapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+        }
+
+        private void ekleBtnM_Click(object sender, EventArgs e)
+        {
+            veriEkleM();
+            VeriGetirM();
+            guncelle();
+            veriCek();
+
+            mNO.Text = "";
+            mName.Text = "";
+            kNO1.Text = "";
+            deadline.Text = "";
+
+        }
+
+        public void veriEkleK()
+        {
+            string sorgu = "Insert into librarypanel2 (kitapisim,stok,yazar,kitapNO) values (@kName,@stok,@azar,@kNO2)";
+
+
+            cmd = new MySqlCommand(sorgu, mysqlbaglan);
+            cmd.Parameters.AddWithValue("@kName", kName.Text);
+            cmd.Parameters.AddWithValue("@stok", stok.Text);
+            cmd.Parameters.AddWithValue("@azar", azar.Text);
+            cmd.Parameters.AddWithValue("@kNO2", kNO2.Text);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Eklendi!");
+
+
+        }
+        public void VeriGetirK()
+        {
+            dt = new DataTable();
+
+            adapter = new MySqlDataAdapter("SELECT *FROM librarypanel2", mysqlbaglan);
+            adapter.Fill(dt);
+            dataGridView2.DataSource = dt;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            veriEkleK();    
+            VeriGetirK();
+            veriCek();
+
+            kName.Text = "";
+            stok.Text = "";
+            azar.Text = "";
+            kNO2.Text = "";
+
+        }
+
+        public void guncelle()
+        {
+            
+
+            adapter = new MySqlDataAdapter("SELECT * FROM librarypanel2 ", mysqlbaglan);
+            MySqlCommand command = new MySqlCommand(
+          "SELECT * FROM librarypanel2 WHERE kitapNO="+kNO1.Text,
+          mysqlbaglan);
+
+
+            MySqlDataReader reader = command.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+
+                    reader.Read();
+                
+                    int tut = Convert.ToInt32(reader["stok"]) - 1;
+                    mysqlbaglan.Close();
+                    mysqlbaglan.Open();
+                    string sql = "UPDATE librarypanel2 SET stok=@stok WHERE kitapNO="+kNO1.Text;
+                    cmd = new MySqlCommand(sql, mysqlbaglan);
+                    cmd.Parameters.AddWithValue("@stok", tut);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Müşteri eklendi!");
+                    VeriGetirK();
+                
+            }
+            else
+            {
+                Console.WriteLine("No rows found.");
+            }
+            reader.Close();
+
+
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            veriCek();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if(kontrol.Contains(kNO2.Text) == true)
+            {
+                string sql = "UPDATE librarypanel2 SET kitapisim=@kName,stok=@stok,yazar=@azar WHERE kitapNO=@kNO2";
+                cmd = new MySqlCommand(sql, mysqlbaglan);
+                cmd.Parameters.AddWithValue("@kName", kName.Text);
+                cmd.Parameters.AddWithValue("@stok", stok.Text);
+                cmd.Parameters.AddWithValue("@azar", azar.Text);
+                cmd.Parameters.AddWithValue("kNO2", kNO2.Text);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Kitap Güncellendi!");
+                VeriGetirK();
+                veriCek();
+            }
+            else
+            {
+                MessageBox.Show("Kitap güncellenemedi. Hata!");
+            }
+
+            
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            veriCek();
+        }
+    }
+}
